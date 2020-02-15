cd418e0452fe1d9b413341a12a3807da6c20001a
Nib04 <hayamin07frst@gmail.com>
2020-02-15 18:20:44 +0900

    add new setting

--- project/settings.py
+++ project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+# heroku用追加部分
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # 管理サイトのログイン機能を通常のログイン機能として使う
 LOGIN_URL = 'admin:login'
@@ -128,3 +131,5 @@ LOGOUT_REDIRECT_URL = '/'
 # django-crispy-forms 設定
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
+# heroku用追加部分
+django_heroku.settings(locals())
